8876df00f95c8a9b998a4894d7e2c3c094e0b1a3
Sumi Akter <sumiwdcse@gmail.com>
2025-09-15 01:28:16 +0600

    module 35 storage  practice

--- storage.js
+++ storage.js
@@ -1,4 +1,14 @@
 const handleAddToStorage = () =>{
     const name = document.getElementById("name").value;
-    localStorage.setItem(name);
+    // localStorage.setItem(name);
+    const id = document.getElementById("id").value;
+    const data = {id , name}
+    localStorage.setItem( id, JSON.stringify(data))
+
+}
+const storeItem = localStorage.getItem("1234");
+console.log(JSON.parse(storeItem))
+
+const handleClear = () => {
+    localStorage.clear()
 }
